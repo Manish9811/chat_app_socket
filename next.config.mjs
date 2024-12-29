2c5f6e7d3fefad3cb4e630f5fd52ef59da325d8b
Manish9811 <107361469+Manish9811@users.noreply.github.com>
2024-12-29 20:51:20 +1100

    debugging

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
-    output: 'standalone',
-  };
-
-export default nextConfig;
-
-
-  
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    /* config options here */
+  }
+   
+  export default nextConfig
