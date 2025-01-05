83794cce24b71d32c0a1f92ab29692b0ef63a2cd
Manish9811 <107361469+Manish9811@users.noreply.github.com>
2025-01-05 16:39:30 +1100

    update socket link

--- app/socket.js
+++ app/socket.js
@@ -2,4 +2,4 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io(process.env.NODE_ENV == 'production' ? 'wss://socketapp-11814d460297.herokuapp.com':"http://localhost:3000")
+export const socket = io('wss://socketapp-11814d460297.herokuapp.com')
